67a9eb0732ace0850d2e6dcf582301d4637b2207
Nofrialdi <aldi.fasive@gmail.com>
2023-11-16 13:04:35 +0700

    feat(config mui): config mui,add image welcome, add page welcome and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,37 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button, Container, Typography } from "@mui/material";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<Container
+			maxWidth="sm"
+			sx={{
+				alignItems: "center",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				height: "100vh",
+				textAlign: "center",
+				width: "320px",
+			}}
+		>
+			<Typography variant="h4" component="h4">
+				AgroMoney
+			</Typography>
+			<Image src="/images/welcome.png" alt="logo" width={275} height={275} />
+			<Typography variant="h5" component="h5" sx={{ mt: 2, textAlign: "center" }}>
+				Selamat Datang
+			</Typography>
+			<Typography variant="caption" sx={{ mt: 2, textAlign: "center" }}>
+				Di AgroMoney, Kami membantu anda mengubah lahan menjadi kesuksesan
+			</Typography>
+			<Button variant="contained" sx={{ mt: 2, width: "100%" }}>
+				Login
+			</Button>
+			<Button variant="outlined" sx={{ mt: 2, width: "100%" }}>
+				Register
+			</Button>
+		</Container>
+	);
 }
