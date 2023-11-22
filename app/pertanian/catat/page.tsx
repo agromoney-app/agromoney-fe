bb8db66e83eae49683533f8efb04a816c1e824d8
Nofrialdi <aldi.fasive@gmail.com>
2023-11-22 18:03:30 +0700

    style(catat-pertanian): style catat-pertanian

--- app/pertanian/catat/page.tsx
+++ app/pertanian/catat/page.tsx
@@ -1,5 +1,19 @@
 "use client";
-import { Box, Button, InputAdornment, TextField, Typography } from "@mui/material";
+import {
+	Box,
+	Button,
+	Container,
+	FormControl,
+	InputAdornment,
+	InputLabel,
+	MenuItem,
+	Paper,
+	Select,
+	SelectChangeEvent,
+	Stack,
+	TextField,
+	Typography,
+} from "@mui/material";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { useRouter } from "next/navigation";
 
@@ -7,101 +21,128 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import React from "react";
+import React, { useState } from "react";
 import { Dayjs } from "dayjs";
-import Navigation from "@/app/components/navigation";
+import Navigation from "@/app/components/navigation2";
 import Shortcut from "@/app/components/shortcut";
+import { ToastContainer } from "react-toastify";
 
 export default function CatatPertanian() {
-	const [tanam, setTanam] = React.useState<Dayjs | null>(null);
-	const [panen, setPanen] = React.useState<Dayjs | null>(null);
+	const [selectedProduct, setSelectedProduct] = useState("");
+
+	const handleChange = (event: SelectChangeEvent) => {
+		setSelectedProduct(event.target.value as string);
+	};
+
 	const router = useRouter();
+
+	const products = [
+		{ id: 0, value: "Bulir" },
+		{ id: 1, value: "Biji minyak" },
+		{ id: 2, value: "Buah dan sayur" },
+		{ id: 3, value: "Kacang-kacangan" },
+		{ id: 4, value: "Gula dan Pemanis" },
+		{ id: 5, value: "Kopi dan Teh" },
+		{ id: 6, value: "Rempah-rempah" },
+	];
 	return (
-		<Box sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-			<Box
+		<Stack
+			display={"flex"}
+			flexDirection={"column"}
+			alignItems={"center"}
+			sx={{ width: "100vw", height: "100vh" }}
+		>
+			{/* TOP BAR */}
+			<Paper
+				square
 				sx={{
 					bgcolor: "primary.main",
-					height: 45,
-					width: "100%",
-					alignItems: "center",
-					p: 0,
-					boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-					zIndex: 2,
-					position: "fixed",
+
+					width: "100vw",
+					zIndex: 50,
 				}}
 			>
-				<Button
-					variant="text"
-					startIcon={<ArrowBackIosNewIcon />}
-					sx={{ bgcolor: "primary.main", color: "white" }}
-					onClick={() => router.push("/home")}
-				>
-					Catat
-				</Button>
-			</Box>
+				<Container maxWidth={"sm"} sx={{ display: "flex", justifyContent: "space-between" }}>
+					<Button
+						onClick={() => router.push("/")}
+						startIcon={<ArrowBackIosNewIcon />}
+						variant="text"
+						sx={{ color: "#ffffff" }}
+					>
+						Catat Pertanian
+					</Button>
+				</Container>
+			</Paper>
 
+			{/* CONTENT */}
+			<Typography
+				variant="h6"
+				component="h6"
+				sx={{ textAlign: "center", color: "primary.main", my: 3 }}
+			>
+				Catat Data Pertanian
+			</Typography>
 			<Box
+				component="form"
 				sx={{
-					mt: 8,
-					width: 320,
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
+					"& .MuiTextField-root": { width: "100%" },
 				}}
+				noValidate
+				autoComplete="off"
 			>
-				<Typography variant="h6" component="h6" sx={{ textAlign: "center", color: "primary.main" }}>
-					Catat Data Pertanian
-				</Typography>
+				<FormControl sx={{ my: 1 }} fullWidth>
+					<InputLabel id="produkSelected">Produk</InputLabel>
+					<Select
+						onChange={handleChange}
+						value={selectedProduct}
+						labelId="produkSelected"
+						label="Produk"
+					>
+						{products.map((product) => (
+							<MenuItem key={product.id} value={product.value}>
+								{product.value}
+							</MenuItem>
+						))}
+					</Select>
+				</FormControl>
 
-				<Box
-					component="form"
-					sx={{
-						"& .MuiTextField-root": { width: 320, display: "flex", flexDirection: "column" },
-					}}
-					noValidate
-					autoComplete="off"
-				>
-					<TextField required sx={{ mt: 3 }} fullWidth label="Produk" id="produk" type="text" />
-
+				<Stack sx={{ my: 1 }} direction={"row"} justifyContent={"space-between"} gap={2}>
 					<LocalizationProvider dateAdapter={AdapterDayjs}>
-						<DemoContainer components={["DatePicker"]}>
-							<DatePicker
-								views={["year", "month", "day"]}
-								format="DD-MM-YYYY"
-								label="Tanggal Tanam"
-							/>
-						</DemoContainer>
+						<DatePicker label="Tanggal Tanam" />
 					</LocalizationProvider>
-
-					<LocalizationProvider dateAdapter={AdapterDayjs}>
-						<DemoContainer components={["DatePicker"]}>
-							<DatePicker
-								views={["year", "month", "day"]}
-								format="DD-MM-YYYY"
-								label="Tanggal Panen"
-							/>
-						</DemoContainer>
-					</LocalizationProvider>
-
-					<TextField sx={{ mt: 1 }} fullWidth label="Catatan" id="catatan" type="text" />
-
-					<TextField
-						label="Jumlah Panen (kosongkan jika belum  panen)"
-						id="jumlah panen"
-						sx={{ mt: 1 }}
-						InputProps={{
-							endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+					<Typography
+						variant="h6"
+						component="h6"
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
 						}}
-					/>
-				</Box>
+					>
+						-
+					</Typography>
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<DatePicker label="Tanngal Panen" />
+					</LocalizationProvider>
+				</Stack>
 
-				<Button fullWidth variant="contained" sx={{ mt: 2 }}>
+				<TextField sx={{ my: 1 }} fullWidth label="Catatan" id="catatan" type="text" />
+				<TextField
+					sx={{ my: 1 }}
+					fullWidth
+					label="Jumlah Panen (kosongkan jika belum  panen)"
+					id="jumlah panen"
+					type="number"
+					InputProps={{
+						endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+					}}
+				/>
+
+				<Button type="submit" variant="contained" sx={{ width: "100%", my: 1 }}>
 					Simpan
 				</Button>
 			</Box>
-
-			<Shortcut />
 			<Navigation />
-		</Box>
+		</Stack>
 	);
 }
