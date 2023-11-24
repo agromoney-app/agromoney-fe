401e2ba353ed65f4de47220f11e72861598b4627
Nofrialdi <aldi.fasive@gmail.com>
2023-11-24 17:45:51 +0700

    feat(catat-keuanagan): add modal in page catat pengeluaran and pemasukan

--- app/keuangan/catat/pemasukan/page.tsx
+++ app/keuangan/catat/pemasukan/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import {
-  Autocomplete,
-  Box,
-  Button,
-  ButtonGroup,
-  Container,
-  MenuItem,
-  Modal,
-  Select,
-  Stack,
-  TextField,
-  Typography,
+	Autocomplete,
+	Box,
+	Button,
+	ButtonGroup,
+	Container,
+	MenuItem,
+	Modal,
+	Select,
+	Stack,
+	TextField,
+	Typography,
 } from "@mui/material";
 import TopBar from "@/app/components/TopBar";
 import { useState } from "react";
@@ -25,48 +25,58 @@ import style from "styled-jsx/style";
 import dayjs, { Dayjs } from "dayjs";
 
 export default function Page() {
-  const [active, setActive] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
+	const [active, setActive] = useState("");
+	const [selectedCategory, setSelectedCategory] = useState("");
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Transaction>();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const [date, setDate] = useState<Date | null>();
-  const handleDateChange = (date: Date | null) => {
-    setDate(date);
-  };
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const submit: SubmitHandler<Transaction> = (data) =>
-    console.log({ date: date, description: data.description });
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    // PAGE
-    <Stack
-      display={"flex"}
-      flexDirection={"column"}
-      alignItems={"center"}
-      padding={0}
-      height={"100vh"}
-    >
-      {/* TOPBAR */}
-      <TopBar />
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Transaction>();
 
-      {/* CONTENT */}
-      <Stack
-        maxWidth={"sm"}
-        width={1}
-        height={1}
-        bgcolor={"#FFFFFF"}
-        padding={2}
-        alignItems={"center"}
-        paddingBottom={10}
-      >
-        {/* <Stack
+	const [date, setDate] = useState<Date | null>();
+	const handleDateChange = (date: Date | null) => {
+		setDate(date);
+	};
+
+	const submit: SubmitHandler<Transaction> = (data) =>
+		console.log({ date: date, description: data.description });
+
+	return (
+		// PAGE
+		<Stack
+			display={"flex"}
+			flexDirection={"column"}
+			alignItems={"center"}
+			padding={0}
+			height={"100vh"}
+		>
+			{/* TOPBAR */}
+			<TopBar />
+
+			{/* CONTENT */}
+			<Stack
+				maxWidth={"sm"}
+				width={1}
+				height={1}
+				bgcolor={"#FFFFFF"}
+				padding={2}
+				alignItems={"center"}
+				paddingBottom={10}
+			>
+				{/* <Stack
           direction={"column"}
           justifyContent={"center"}
           alignItems={"center"}
@@ -74,75 +84,90 @@ export default function Page() {
           padding={2}
           height={1}
         > */}
-        {/* TOP */}
-        <Typography variant="h6" color={"primary.main"} marginBottom={2}>
-          Transaksi Baru
-        </Typography>
-        <ButtonGroup>
-          <Button
-            onClick={() =>
-              (window.location.href = "/keuangan/catat/pengeluaran")
-            }
-          >
-            Pengeluaran
-          </Button>
-          <Button variant="contained">Pemasukan</Button>
-        </ButtonGroup>
+				{/* TOP */}
+				<Typography variant="h6" color={"primary.main"} marginBottom={2}>
+					Transaksi Baru
+				</Typography>
+				<ButtonGroup>
+					<Button onClick={() => (window.location.href = "/keuangan/catat/pengeluaran")}>
+						Pengeluaran
+					</Button>
+					<Button variant="contained">Pemasukan</Button>
+				</ButtonGroup>
 
-        {/* VALUE */}
-        <Typography
-          onClick={() => setModalOpen(true)}
-          variant="h4"
-          color={"primary.main"}
-          marginTop={"auto"}
-        >
-          Rp 0
-        </Typography>
+				{/* VALUE */}
+				<Typography
+					onClick={handleOpenModal}
+					variant="h4"
+					color={"primary.main"}
+					marginTop={"auto"}
+				>
+					Rp 0
+				</Typography>
 
-        {/* MODAL */}
-        <Modal
-          open={modalOpen}
-          onClose={() => setModalOpen(false)}
-          aria-labelledby="child-modal-title"
-          aria-describedby="child-modal-description"
-        >
-          <Box sx={{ ...style, width: 200 }}></Box>
-        </Modal>
+				{/* MODAL */}
+				<Modal open={isModalOpen} onClose={handleCloseModal}>
+					<Box
+						maxWidth={"sm"}
+						sx={{
+							position: "absolute",
+							top: "50%",
+							left: "50%",
+							transform: "translate(-50%, -50%)",
+							width: 400,
+							bgcolor: "background.paper",
+							borderRadius: "10px",
+							boxShadow: 24,
+							p: 4,
+							display: "flex",
+							flexDirection: "column",
+						}}
+					>
+						<Typography sx={{ my: 1 }} id="modal-modal-title" variant="h6" component="h2">
+							Jumlah Pemasukan
+						</Typography>
+						<TextField
+							sx={{ my: 1 }}
+							id="outlined-basic"
+							label="Jumlah Pemasukan"
+							variant="outlined"
+						/>
+						<Button sx={{ my: 1 }} variant="contained">
+							Simpan
+						</Button>
+					</Box>
+				</Modal>
 
-        {/* FORM */}
-        <Stack direction={"column"} width={"100%"} gap={2} marginTop={"auto"}>
-          {/* DATE */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={["DatePicker"]}>
-              <DatePicker
-                label="Tanggal transaksi"
-                value={date}
-                onChange={(newDate) => setDate(newDate)}
-                format="DD/MM/YYYY"
-              />
-            </DemoContainer>
-          </LocalizationProvider>
+				{/* FORM */}
+				<Stack direction={"column"} width={"100%"} gap={2} marginTop={"auto"}>
+					{/* DATE */}
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<DemoContainer components={["DatePicker"]}>
+							<DatePicker
+								label="Tanggal transaksi"
+								value={date}
+								onChange={(newDate) => setDate(newDate)}
+								format="DD/MM/YYYY"
+							/>
+						</DemoContainer>
+					</LocalizationProvider>
 
-          {/* DESKRIPSI */}
-          <TextField
-            id="description"
-            label="Deskripsi (opsional)"
-            variant="outlined"
-            fullWidth
-            defaultValue={""}
-            {...register("description")}
-          />
-          <Button
-            variant="contained"
-            size="large"
-            onClick={handleSubmit(submit)}
-          >
-            Simpan
-          </Button>
-        </Stack>
-        {/* </Stack> */}
-      </Stack>
-      <Navigation />
-    </Stack>
-  );
+					{/* DESKRIPSI */}
+					<TextField
+						id="description"
+						label="Deskripsi (opsional)"
+						variant="outlined"
+						fullWidth
+						defaultValue={""}
+						{...register("description")}
+					/>
+					<Button variant="contained" size="large" onClick={handleSubmit(submit)}>
+						Simpan
+					</Button>
+				</Stack>
+				{/* </Stack> */}
+			</Stack>
+			<Navigation />
+		</Stack>
+	);
 }
--- app/keuangan/catat/pengeluaran/page.tsx
+++ app/keuangan/catat/pengeluaran/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import {
-  Autocomplete,
-  Box,
-  Button,
-  ButtonGroup,
-  Container,
-  MenuItem,
-  Select,
-  Stack,
-  TextField,
-  Typography,
+	Autocomplete,
+	Box,
+	Button,
+	ButtonGroup,
+	Container,
+	MenuItem,
+	Modal,
+	Select,
+	Stack,
+	TextField,
+	Typography,
 } from "@mui/material";
 import TopBar from "@/app/components/TopBar";
 import { useState } from "react";
@@ -22,63 +23,74 @@ import { Transaction } from "@/app/interfaces/interface";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 export default function Page() {
-  const [active, setActive] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState("");
+	const [active, setActive] = useState("");
+	const [selectedCategory, setSelectedCategory] = useState("");
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Transaction>();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const [date, setDate] = useState<Date | null>();
-  const handleDateChange = (date: Date | null) => {
-    setDate(date);
-  };
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Transaction>();
 
-  const submit: SubmitHandler<Transaction> = (data) =>
-    console.log({ date: date, description: data.description });
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const categories = [
-    { id: 0, label: "Benih dan Tanaman" },
-    { id: 1, label: "Pupuk dan Pembenah Tanah" },
-    { id: 2, label: "Pestisida dan Herbisida" },
-    { id: 3, label: "Peralatan dan Mesin" },
-    { id: 4, label: "Bahan Bakar dan Energi" },
-    { id: 5, label: "Tenaga Kerja" },
-    { id: 6, label: "Air" },
-    { id: 7, label: "Perbaikan dan Pemeliharaan" },
-    { id: 8, label: "Pengemasan dan Transportasi" },
-    { id: 9, label: "Asuransi" },
-    { id: 10, label: "Pemasaran dan Penjualan" },
-    { id: 11, label: "Utilitas" },
-    { id: 12, label: "Pendidikan dan Pelatihan" },
-    { id: 13, label: "Pajak dan Izin" },
-  ];
-  return (
-    // PAGE
-    <Stack
-      display={"flex"}
-      flexDirection={"column"}
-      alignItems={"center"}
-      padding={0}
-      height={"100vh"}
-    >
-      {/* TOPBAR */}
-      <TopBar />
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
 
-      {/* CONTENT */}
-      <Stack
-        maxWidth={"sm"}
-        width={1}
-        height={1}
-        bgcolor={"#FFFFFF"}
-        padding={2}
-        alignItems={"center"}
-        paddingBottom={10}
-      >
-        {/* <Stack
+	const [date, setDate] = useState<Date | null>();
+	const handleDateChange = (date: Date | null) => {
+		setDate(date);
+	};
+
+	const submit: SubmitHandler<Transaction> = (data) =>
+		console.log({ date: date, description: data.description });
+
+	const categories = [
+		{ id: 0, label: "Benih dan Tanaman" },
+		{ id: 1, label: "Pupuk dan Pembenah Tanah" },
+		{ id: 2, label: "Pestisida dan Herbisida" },
+		{ id: 3, label: "Peralatan dan Mesin" },
+		{ id: 4, label: "Bahan Bakar dan Energi" },
+		{ id: 5, label: "Tenaga Kerja" },
+		{ id: 6, label: "Air" },
+		{ id: 7, label: "Perbaikan dan Pemeliharaan" },
+		{ id: 8, label: "Pengemasan dan Transportasi" },
+		{ id: 9, label: "Asuransi" },
+		{ id: 10, label: "Pemasaran dan Penjualan" },
+		{ id: 11, label: "Utilitas" },
+		{ id: 12, label: "Pendidikan dan Pelatihan" },
+		{ id: 13, label: "Pajak dan Izin" },
+	];
+	return (
+		// PAGE
+		<Stack
+			display={"flex"}
+			flexDirection={"column"}
+			alignItems={"center"}
+			padding={0}
+			height={"100vh"}
+		>
+			{/* TOPBAR */}
+			<TopBar />
+
+			{/* CONTENT */}
+			<Stack
+				maxWidth={"sm"}
+				width={1}
+				height={1}
+				bgcolor={"#FFFFFF"}
+				padding={2}
+				alignItems={"center"}
+				paddingBottom={10}
+			>
+				{/* <Stack
           direction={"column"}
           justifyContent={"center"}
           alignItems={"center"}
@@ -86,74 +98,96 @@ export default function Page() {
           padding={2}
           height={1}
         > */}
-        {/* TOP */}
-        <Typography variant="h6" color={"primary.main"} marginBottom={2}>
-          Transaksi Baru
-        </Typography>
-        <ButtonGroup>
-          <Button variant="contained">Pengeluaran</Button>
-          <Button
-            onClick={() => (window.location.href = "/keuangan/catat/pemasukan")}
-          >
-            Pemasukan
-          </Button>
-        </ButtonGroup>
+				{/* TOP */}
+				<Typography variant="h6" color={"primary.main"} marginBottom={2}>
+					Transaksi Baru
+				</Typography>
+				<ButtonGroup>
+					<Button variant="contained">Pengeluaran</Button>
+					<Button onClick={() => (window.location.href = "/keuangan/catat/pemasukan")}>
+						Pemasukan
+					</Button>
+				</ButtonGroup>
+				{/* VALUE */}
+				{/* <Box marginY={5}> */}
+				<Typography
+					onClick={handleOpenModal}
+					variant="h4"
+					color={"primary.main"}
+					marginTop={"auto"}
+				>
+					Rp 0
+				</Typography>
 
-        {/* VALUE */}
-        {/* <Box marginY={5}> */}
-        <Typography
-          onClick={() => alert("Clicked")}
-          variant="h4"
-          color={"primary.main"}
-          marginTop={"auto"}
-        >
-          Rp 0
-        </Typography>
+				{/* MODAL */}
+				<Modal open={isModalOpen} onClose={handleCloseModal}>
+					<Box
+						maxWidth={"sm"}
+						sx={{
+							position: "absolute",
+							top: "50%",
+							left: "50%",
+							transform: "translate(-50%, -50%)",
+							width: 400,
+							bgcolor: "background.paper",
+							borderRadius: "10px",
+							boxShadow: 24,
+							p: 4,
+							display: "flex",
+							flexDirection: "column",
+						}}
+					>
+						<Typography sx={{ my: 1 }} id="modal-modal-title" variant="h6" component="h2">
+							Jumlah Pengeluaran
+						</Typography>
+						<TextField
+							sx={{ my: 1 }}
+							id="outlined-basic"
+							label="Jumlah Pengeluaran"
+							variant="outlined"
+						/>
+						<Button sx={{ my: 1 }} variant="contained">
+							Simpan
+						</Button>
+					</Box>
+				</Modal>
+				{/* FORM */}
+				<Stack direction={"column"} width={"100%"} gap={2} marginTop={"auto"}>
+					{/* DATE */}
 
-        {/* FORM */}
-        <Stack direction={"column"} width={"100%"} gap={2} marginTop={"auto"}>
-          {/* DATE */}
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<DemoContainer components={["DatePicker"]}>
+							<DatePicker
+								label="Tanggal transaksi"
+								value={date}
+								onChange={(newDate) => setDate(newDate)}
+							/>
+						</DemoContainer>
+					</LocalizationProvider>
+					{/* JENIS */}
+					<Select value={selectedCategory} label="Jenis pengeluaran">
+						{categories.map((category) => (
+							<MenuItem key={category.id} onClick={() => setSelectedCategory(category.label)}>
+								{category.label}
+							</MenuItem>
+						))}
+					</Select>
 
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={["DatePicker"]}>
-              <DatePicker
-                label="Tanggal transaksi"
-                value={date}
-                onChange={(newDate) => setDate(newDate)}
-              />
-            </DemoContainer>
-          </LocalizationProvider>
-          {/* JENIS */}
-          <Select value={selectedCategory} label="Jenis pengeluaran">
-            {categories.map((category) => (
-              <MenuItem
-                key={category.id}
-                onClick={() => setSelectedCategory(category.label)}
-              >
-                {category.label}
-              </MenuItem>
-            ))}
-          </Select>
-
-          {/* DESKRIPSI */}
-          <TextField
-            id="description"
-            label="Deskripsi (opsional)"
-            variant="outlined"
-            fullWidth
-            defaultValue={""}
-            {...register("description")}
-          />
-          <Button
-            variant="contained"
-            size="large"
-            onClick={handleSubmit(submit)}
-          >
-            Simpan
-          </Button>
-        </Stack>
-      </Stack>
-      <Navigation />
-    </Stack>
-  );
+					{/* DESKRIPSI */}
+					<TextField
+						id="description"
+						label="Deskripsi (opsional)"
+						variant="outlined"
+						fullWidth
+						defaultValue={""}
+						{...register("description")}
+					/>
+					<Button variant="contained" size="large" onClick={handleSubmit(submit)}>
+						Simpan
+					</Button>
+				</Stack>
+			</Stack>
+			<Navigation />
+		</Stack>
+	);
 }
