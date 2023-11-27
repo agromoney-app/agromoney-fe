8e2c4ad49b89ed98cbb7c96475a3a72707e0ffd2
Nofrialdi <aldi.fasive@gmail.com>
2023-11-27 21:00:34 +0700

    fix(deploy): fix interface home

--- app/interfaces/interface.ts
+++ app/interfaces/interface.ts
@@ -1,20 +1,20 @@
 export interface Transaction {
-  date: Date | string | any;
-  description: string;
+	date: Date | string | any;
+	description: string;
 }
 
 export interface Product {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 export interface Yield {
-  id: number;
-  productId: number;
-  product: string;
-  plantingTime: string;
-  harvestTime: any;
-  description: string;
-  quantity: number;
-  isHarvested: boolean;
+	id: number;
+	productId: number;
+	plantingTime: string;
+	harvestTime: any;
+	description: string;
+	quantity: number;
+	isHarvested: boolean;
+	product: Product;
 }
